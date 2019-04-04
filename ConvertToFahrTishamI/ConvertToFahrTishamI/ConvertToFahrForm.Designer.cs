6bce857709be8ba707afe48548b5350f017eb0d9
Tisham Islam <45071113+sl1mt1sh@users.noreply.github.com>
2019-04-04 12:47:17 -0400

    Initial commit

---
+++ ConvertToFahrTishamI/ConvertToFahrTishamI/ConvertToFahrForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ConvertToFahrTishamI
+{
+    partial class frmConvertToFahr
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblConversion = new System.Windows.Forms.Label();
+            this.lblEnterTemp = new System.Windows.Forms.Label();
+            this.txtTemp = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblConversion
+            // 
+            this.lblConversion.AutoSize = true;
+            this.lblConversion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConversion.Location = new System.Drawing.Point(36, 311);
+            this.lblConversion.Name = "lblConversion";
+            this.lblConversion.Size = new System.Drawing.Size(156, 25);
+            this.lblConversion.TabIndex = 0;
+            this.lblConversion.Text = "Hidden for now";
+            // 
+            // lblEnterTemp
+            // 
+            this.lblEnterTemp.AutoSize = true;
+            this.lblEnterTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterTemp.Location = new System.Drawing.Point(36, 26);
+            this.lblEnterTemp.Name = "lblEnterTemp";
+            this.lblEnterTemp.Size = new System.Drawing.Size(676, 25);
+            this.lblEnterTemp.TabIndex = 1;
+            this.lblEnterTemp.Text = "Enter a temperature in Celsius and it shall be converted to Fahrenheit";
+            // 
+            // txtTemp
+            // 
+            this.txtTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTemp.Location = new System.Drawing.Point(41, 72);
+            this.txtTemp.Name = "txtTemp";
+            this.txtTemp.Size = new System.Drawing.Size(671, 44);
+            this.txtTemp.TabIndex = 2;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(41, 141);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(671, 151);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "CONVERT";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // frmConvertToFahr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtTemp);
+            this.Controls.Add(this.lblEnterTemp);
+            this.Controls.Add(this.lblConversion);
+            this.Name = "frmConvertToFahr";
+            this.Text = "Convert To Fahr by Tisham Islam";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblConversion;
+        private System.Windows.Forms.Label lblEnterTemp;
+        private System.Windows.Forms.TextBox txtTemp;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ ConvertToFahrTishamI/ConvertToFahrTishamI/ConvertToFahrForm.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Created by: Tisham Islam
+ * Created on: 04/04/2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #28 - Convert To Fahrenheit
+ * This program converts the inputted temperature from Celsius to Fahrenheit
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertToFahrTishamI
+{
+    public partial class frmConvertToFahr : Form
+    {
+        public frmConvertToFahr()
+        {
+            InitializeComponent();
+            lblConversion.Hide();
+        }
+
+        //Procedure: ConvertToFahr
+        //Input: celsiusTemp
+        //Output: void
+        //This will convert degrees Celsius to degrees Fahrenheit
+        private void ConvertToFahr(decimal celsiusTemp)
+        {
+            //variables
+            decimal fahrenheitTemp;
+
+            //Calculations for Cel to Fahr
+            fahrenheitTemp = celsiusTemp * (decimal)9 / (decimal)5 + 32;
+
+            //Display the conversion
+            lblConversion.Text = "Here is your temperature in degrees Fahrenheit: " + fahrenheitTemp;
+            lblConversion.Show();
+        }
+
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            //variables
+            decimal userTemp;
+
+            //Make userTemp into your input for temperature
+            userTemp = Convert.ToDecimal(txtTemp.Text);
+
+            //Call the procedure
+            this.ConvertToFahr(userTemp);
+
+        }
+    }
+}
